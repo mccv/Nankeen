71c0453772891290fba9f4a4e92e22be73d0a351
mmcbride <mccv@twitter.com>
2010-07-30 11:36:13 -0700

    reducing to a single smile client per loader, cleaning up build, switching to StandardProject

--- src/main/scala/com/twitter/nankeen/Nankeen.scala
+++ src/main/scala/com/twitter/nankeen/Nankeen.scala
@@ -8,13 +8,20 @@ import net.lag.smile._
 import scala.collection.mutable._
 
 class Reader(client: MemcacheClient[String], queueName: String, numMessages: Int, expectedMessages: Set[String]) extends Runnable{
+  val log = Logger.get("nankeen")
 
   def run() = {
-    while (expectedMessages.size > 0) {
-      get()
-      Thread.sleep(10)
+    try {
+      while (expectedMessages.size > 0) {
+        get()
+        Thread.sleep(10)
+      }
+    } catch {
+      case e => {
+        log.error("error reading messages: %s", e)
+        e.printStackTrace()
+      }
     }
-    client.shutdown()
   }
 
   def get() = {
@@ -30,15 +37,25 @@ class Reader(client: MemcacheClient[String], queueName: String, numMessages: Int
 }
 
 class Writer(client: MemcacheClient[String], queueName: String, numMessages: Int, messages: LinkedBlockingQueue[String]) extends Runnable{
+  val log = Logger.get("nankeen")
 
   def run(): Unit = {
-    while (true) {
-      val msg = messages.poll()
-      if (msg == null) {
-        client.shutdown()
+    try {
+      while (true) {
+        val msg = messages.poll()
+        if (msg == null) {
+          return
+        }
+        put(msg)
+      }
+    } catch {
+      case e => {
+        // clear these out so readers stop
+        log.error("error writing messages: %s", e)
+        e.printStackTrace()
+        messages.clear
         return
       }
-      put(msg)
     }
   }
 
@@ -52,23 +69,21 @@ class Loader(servers: Array[String], queueName: String, numWriters: Int,
 
   val distribution = "ketama"
   val hash = "fnv1a-64"
-  def client() = {
-    val locator = NodeLocator.byName(distribution) match {
-      case (hashName, factory) =>
-        factory(KeyHasher.byName(hash))
-    }
-    val client = new MemcacheClient(locator, MemcacheCodec.UTF8)
-    val pool = new ServerPool
-    val connections = for (s <- servers) yield ServerPool.makeConnection(s, pool)
-    pool.servers = connections
-    client.setPool(pool)
-    client
-  }
 
   val messagesSet = Set((1 to numMessages).map(i => Nankeen.messagePrefix + i):_*)
 
   val messagesQueue = new LinkedBlockingQueue[String](messagesSet.size)
   messagesSet.foreach(messagesQueue.offer(_))
+
+  val locator = NodeLocator.byName(distribution) match {
+    case (hashName, factory) =>
+      factory(KeyHasher.byName(hash))
+  }
+  val client = new MemcacheClient(locator, MemcacheCodec.UTF8)
+  val pool = new ServerPool
+  val connections = for (s <- servers) yield ServerPool.makeConnection(s, pool)
+  pool.servers = connections
+  client.setPool(pool)
 
   val readers = (1 to numReaders).map(i => new Reader(client, queueName, numMessages, messagesSet)).toList
   val writers = (1 to numWriters).map(i => new Writer(client, queueName, numMessages, messagesQueue)).toList
@@ -80,6 +95,7 @@ class Loader(servers: Array[String], queueName: String, numWriters: Int,
     writerThreads.foreach(_.start)
     readerThreads.foreach(_.join)
     writerThreads.foreach(_.join)
+    client.shutdown()
   }
 }
 
@@ -87,22 +103,24 @@ object Nankeen extends LoggingLoadTest {
   val messagePrefix = "Nankeen Load Test Message "
   val log = Logger.get("nankeen")
   def main(args: Array[String]) = {
-    if (args.length != 6) {
-      Console.println("usage: general-loader")
+    if (args.length != 7) {
+      Console.println("Nankeen")
       Console.println("    spin up a number of loaders that each")
       Console.println("    spin up M writers and have them write N messages to a queue")
       Console.println("    for Z loops")
       Console.println("    spin up O writers to drain the queue")
-      Console.println("    example: general-loader queue-name 1 10 10 100")
+      Console.println("usage:")
+      Console.println("    java -jar nankeen-0.1.jar localhost:22133 test 10 1 1 1 1")
       System.exit(1)
     }
 
-    val queueName = args(0).toString
-    var loops = args(1).toInt
-    val numQueues = args(2).toInt
-    val numReaders = args(3).toInt
-    val numWriters = args(4).toInt
-    val numMessages = args(5).toInt
+    val hostName = args(0).toString
+    val queueName = args(1).toString
+    var loops = args(2).toInt
+    val numQueues = args(3).toInt
+    val numReaders = args(4).toInt
+    val numWriters = args(5).toInt
+    val numMessages = args(6).toInt
 
     val timingsFile = new File("timings.log")
     log.info("Using %d writers and %d readers to write %d messages to %d queues prefaced by %s".
@@ -112,7 +130,7 @@ object Nankeen extends LoggingLoadTest {
     while(loops != 0) {
       loops -= 1
       val loaderThreads = queues.map {i =>
-        val loader = new Loader(Array("localhost:22133"), queueName + i, numWriters, numReaders, numMessages)
+        val loader = new Loader(Array(hostName), queueName + i, numWriters, numReaders, numMessages)
         new Thread(loader)
       }
       runWithTiming {
