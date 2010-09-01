891d01590a4601cd0cc935d3b501cee9b2b2b166
mmcbride <mccv@twitter.com>
2010-08-31 18:08:06 -0700

    updating standard project version, eliminating log4j deps

--- project/build/NankeenProject.scala
+++ project/build/NankeenProject.scala
@@ -1,10 +1,14 @@
 import sbt._
-import com.twitter.sbt.StandardProject
+import com.twitter.sbt._
 
 class NankeenProject(info: ProjectInfo) extends StandardProject(info) {
   // dependencies
   val jackhammer = "com.twitter" % "jackhammer_2.7.7" % "1.0"
   val smile = "net.lag" % "smile" % "0.8.12"
+  override def ivyXML =
+    <dependencies>
+      <exclude org="apache-log4j"/>
+    </dependencies>
 
   override def mainClass = Some("com.twitter.nankeen.Nankeen")
 }
